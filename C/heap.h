0cd836eb213b8c2c5a35c7f9e871e2bd5c7d1ef1
Bus000 <magnus@stavngaard.dk>
2016-03-20 17:38:52 +0100

    Add comments to function prototypes

--- C/heap.h
+++ C/heap.h
@@ -23,20 +23,31 @@ typedef struct {
     void (*decrease_key)(void *el, void *newkey);
 } min_heap_t;
 
+/* Initialize a new heap in the pointer heap, with the initial elements in the
+ * array, array with the size array_size. The function compare, should take two
+ * elements from the array and return -1 if the first element is less than the
+ * second, 0 if they are equal and 1 if the first element is greater than the
+ * second. */
 int heap_init(min_heap_t *heap, void **array, uint32_t array_size,
         int (*compare)(void const *el1, void const *el2),
         void (*decrease_key)(void *el, void *newkey));
 
+/* Finds the smallest element in the heap, but keeps it in the heap. */
 void * heap_peek_min(min_heap_t const *heap);
 
+/* Find and remove the smallest element in the heap. */
 void * heap_extract_min(min_heap_t *heap);
 
+/* Moves the element with the index given higher in the heap by increasing its
+ * decreasing its key. */
 int heap_decrease_key(min_heap_t *heap, uint32_t index, void *newkey);
 
 /* Locate the pointer el in heap and decrease the key of that pointer to the
  * key given. Returns 0 on success and -1 if the element does not exist. */
 int heap_decrease_element(min_heap_t *heap, void *el, void *newkey);
 
+/* Prints a heap to the file f by providing a printing function for the elements
+ * in the heap. */
 void heap_print(void *h, FILE *f, void (*print_el)(void *, FILE *));
 
 #endif
