660b1cb490b86d7055fce05fceeee382cea0b690
Bus000 <magnus@stavngaard.dk>
2016-03-27 23:08:17 +0200

    Continue implementation of algorithm

--- C/src/thorup_dist.c
+++ C/src/thorup_dist.c
@@ -28,19 +28,25 @@ int thorup_layer(graph_t *graph)
 static int get_two_layer_graphs(graph_t *newgraph, graph_t *oldgraph,
         vertex_t *vertex)
 {
-    vertex_list_t reach;
+    vertex_id_t new;
+    vertex_list_t reachable_list;
+    vertex_list_t reaching_list;
     vertex_t *vertices;
 
-    vertex_list_init(&reach);
+    vertex_list_init(&reachable_list);
+    vertex_list_init(&reaching_list);
 
     /* Make graph of all vertices reachable from start. */
-    reachable(&reach, vertex);
-    vertices = pointer_list_to_list(&reach);
-    graph_init_vertices(newgraph, vertices, reach.len);
-    graph_contract(oldgraph, &reach);
+    reachable(&reachable_list, vertex);
+    vertices = pointer_list_to_list(&reachable_list);
+    graph_init_vertices(newgraph, vertices, reachable_list.len);
+    graph_contract(oldgraph, &reachable_list);
 
     /* Remove all vertices from old graph that are in the newly created
-     * graph replacing them with a single new vertex. */
+     * graph replacing them with a single new vertex, "new". */
+    new = graph_contract(oldgraph, &reachable_list);
+
+    printf("%u\n", new);
 
     /* Find all vertices in the old graph that has a transition into the new
      * graph, add these vertices to the new graph and remove them from the
@@ -50,7 +56,9 @@ static int get_two_layer_graphs(graph_t *newgraph, graph_t *oldgraph,
      * to a single vertex, repeat all steps. */
 
     /* Free resources. */
-    vertex_list_free(&reach);
+    vertex_list_free(&reachable_list);
+    vertex_list_free(&reaching_list);
+    free(vertices);
 
     return 0;
 }
