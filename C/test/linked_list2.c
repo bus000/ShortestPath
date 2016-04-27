b0e09baf9f67e23342e540c066f2f81cfc5e6b66
Bus000 <magnus@stavngaard.dk>
2016-04-27 15:22:16 +0200

    Testing

--- C/test/linked_list2.c
+++ C/test/linked_list2.c
@@ -5,7 +5,7 @@
 int main(int argc, char const *argv[])
 {
     linked_list_t list = linked_list_init();
-    uint32_t n;
+    uint32_t n, n2;
     actual_list_t *alist;
 
     for (n = 0; n < 100; n += 10)
@@ -13,6 +13,20 @@ int main(int argc, char const *argv[])
 
     for (alist = list.start; alist != NULL; alist = alist->next)
         printf("%u\n", alist->int_element);
+
+    linked_list_free(&list);
+
+    /* Make list with one element and get the last one. */
+    list = linked_list_init();
+    n = 1337;
+    n2 = 123;
+    linked_list_add_end(&list, &n);
+    printf("last singular %u\n", *((uint32_t *) linked_list_get(&list, -1)));
+    linked_list_add_end(&list, &n2);
+    linked_list_add_end(&list, &n2);
+    linked_list_remove_last(&list);
+    linked_list_remove_last(&list);
+    printf("last singular %u\n", *((uint32_t *) linked_list_get(&list, -1)));
 
     linked_list_free(&list);
 
