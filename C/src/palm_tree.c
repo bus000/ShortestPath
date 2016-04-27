903890b8a1db0bc767a0a959334f1d940bea70a9
Bus000 <magnus@stavngaard.dk>
2016-04-27 11:18:26 +0200

    Fix small errors

--- C/src/palm_tree.c
+++ C/src/palm_tree.c
@@ -4,17 +4,52 @@
 #include "set.h"
 #include <stdlib.h>
 
-static palm_tree_label_t default_label = { .number = -1, .l = -1, .ll = -1};
+static palm_tree_label_t default_label = { .number = -1, .l = -1, .ll = -1 };
 
-static int edge_hash(void const *e);
-static int edge_cmp(void const *e1, void const *e2);
-static uint32_t edge_score(edge_t const *edge);
+static int edge_hash(void const *e)
+{
+    edge_t const *edge = (edge_t const *) e;
+
+    return edge->start->unique_id + edge->end->unique_id + edge->weight;
+}
+
+static int edge_cmp(void const *e1, void const *e2)
+{
+    edge_t const *edge1 = (edge_t const *) e1;
+    edge_t const *edge2 = (edge_t const *) e2;
+
+    if (edge1->start->unique_id == edge2->start->unique_id &&
+        edge1->end->unique_id == edge2->end->unique_id &&
+        edge1->weight == edge2->weight)
+        return 0;
+    else if (edge1->start->unique_id == edge2->end->unique_id &&
+            edge1->end->unique_id == edge2->start->unique_id &&
+            edge1->weight == edge2->weight)
+        return 0;
+    else
+        return 1;
+}
+
+static uint32_t edge_score(edge_t const *edge)
+{
+    if (palm_tree_frond(edge))
+        return 2 * palm_number(edge->end);
+    else if (palm_tree_arc(edge) &&
+            palm_second_lowest(edge->end) == palm_number(edge->end))
+        return 2 * palm_lowest(edge->end);
+    else if (palm_tree_arc(edge) &&
+            palm_second_lowest(edge->end) < palm_number(edge->end))
+        return 2 * palm_lowest(edge->end) + 1;
+
+
+    fprintf(stderr, "Error should never not hit any of the cases above\n");
+    return -1;
+}
 
 static inline int is_frond(vertex_t const *u, vertex_t const *v,
         vertex_t const *w)
 {
-    return u != NULL &&
-        w->unique_id != u->unique_id &&
+    return w->unique_id != u->unique_id &&
         0 <= PALM_NUMBER(w) &&
         PALM_NUMBER(w) < PALM_NUMBER(v);
 }
@@ -22,14 +57,14 @@ static inline int is_frond(vertex_t const *u, vertex_t const *v,
 static inline int already_directed(vertex_t const *u, vertex_t const *v,
         vertex_t const *w)
 {
-    return (u != NULL && w->unique_id == u->unique_id) ||
-        PALM_NUMBER(v) < PALM_NUMBER(w);
+    return w->unique_id == u->unique_id || PALM_NUMBER(v) < PALM_NUMBER(w);
 }
 
 static void palm_tree_DFS(vertex_t const *u, vertex_t *v, uint32_t *n)
 {
     uint32_t i;
     vertex_t *w;
+    int64_t tmp;
 
     PALM_NUMBER(v) = *n;
     *n = *n + 1;
@@ -44,8 +79,9 @@ static void palm_tree_DFS(vertex_t const *u, vertex_t *v, uint32_t *n)
 
         if (is_frond(u, v, w)) {
             if (PALM_NUMBER(w) < PALM_L(v)) {
-                PALM_LL(v) = PALM_L(v);
+                tmp = PALM_L(v);
                 PALM_L(v) = PALM_NUMBER(w);
+                PALM_LL(v) = tmp;
             } else if (PALM_NUMBER(w) > PALM_L(v)) {
                 PALM_LL(v) = min(PALM_LL(v), PALM_NUMBER(w));
             }
@@ -64,6 +100,8 @@ static void palm_tree_DFS(vertex_t const *u, vertex_t *v, uint32_t *n)
                 PALM_L(v) = PALM_L(w);
             } else if (PALM_L(w) == PALM_L(v) && PALM_L(w) < PALM_LL(w)) {
                 PALM_LL(v) = min(PALM_LL(v), PALM_LL(w));
+            } else if (PALM_L(w) == PALM_L(v) && PALM_L(w) == PALM_LL(w)) {
+                /* Skip. */
             } else if (PALM_L(w) > PALM_L(v)) {
                 PALM_LL(v) = min(PALM_LL(v), PALM_L(w));
             }
@@ -75,8 +113,9 @@ static void palm_tree_DFS(vertex_t const *u, vertex_t *v, uint32_t *n)
 
         if (is_frond(u, v, w)) {
             if (PALM_NUMBER(w) < PALM_L(v)) {
-                PALM_LL(v) = PALM_L(v);
+                tmp = PALM_L(v);
                 PALM_L(v) = PALM_NUMBER(w);
+                PALM_LL(v) = tmp;
             } else if (PALM_NUMBER(w) > PALM_L(v)) {
                 PALM_LL(v) = min(PALM_LL(v), PALM_NUMBER(w));
             }
@@ -95,6 +134,8 @@ static void palm_tree_DFS(vertex_t const *u, vertex_t *v, uint32_t *n)
                 PALM_L(v) = PALM_L(w);
             } else if (PALM_L(w) == PALM_L(v) && PALM_L(w) < PALM_LL(w)) {
                 PALM_LL(v) = min(PALM_LL(v), PALM_LL(w));
+            } else if (PALM_L(w) == PALM_L(v) && PALM_L(w) == PALM_LL(w)) {
+                /* Skip. */
             } else if (PALM_L(w) > PALM_L(v)) {
                 PALM_LL(v) = min(PALM_LL(v), PALM_L(w));
             }
@@ -113,7 +154,7 @@ static void sort_adjasent(digraph_t *graph)
 
     edges_back = malloc(sizeof(edge_t) * graph->edges_len);
     edges_end = edges_back;
-    if (edges_back == NULL)
+    if (edges_back == NULL || bucket == NULL)
         mem_err();
 
     for (i = 0; i < buckets; i++)
@@ -141,7 +182,6 @@ static void sort_adjasent(digraph_t *graph)
     }
 
     for (i = 0; i < buckets; i++) {
-        edges = set_get_contents(&bucket[i]).start;
         for (edges = set_get_contents(&bucket[i]).start; edges != NULL;
                 edges = edges->next) {
             edge = edges->element;
@@ -170,8 +210,18 @@ int palm_tree(digraph_t *graph)
 
     first_vertex = graph->vertices[0];
     PALM_NUMBER(first_vertex) = 0;
+    PALM_L(first_vertex) = 0;
+    PALM_LL(first_vertex) = 0;
 
-    palm_tree_DFS(NULL, first_vertex, &n);
+    if (first_vertex->outgoing_len != 0) {
+        palm_tree_DFS(first_vertex, first_vertex->outgoing[0].end, &n);
+        first_vertex->outgoing_len = 1;
+        first_vertex->incoming_len = 0;
+    } else {
+        palm_tree_DFS(first_vertex, first_vertex->incoming[0].end, &n);
+        first_vertex->incoming_len = 1;
+    }
+
     sort_adjasent(graph);
 
     return 0;
@@ -200,37 +250,4 @@ int64_t inline palm_lowest(vertex_t const *vertex)
 int64_t inline palm_second_lowest(vertex_t const *vertex)
 {
     return PALM_LL(vertex);
-}
-
-static int edge_hash(void const *e)
-{
-    edge_t const *edge = (edge_t const *) e;
-
-    return edge->start->unique_id + edge->end->unique_id + edge->weight;
-}
-
-static int edge_cmp(void const *e1, void const *e2)
-{
-    edge_t const *edge1 = (edge_t const *) e1;
-    edge_t const *edge2 = (edge_t const *) e2;
-
-    return edge1->start->unique_id == edge2->start->unique_id &&
-        edge1->end->unique_id == edge2->end->unique_id &&
-        edge1->weight == edge2->weight;
-}
-
-static uint32_t edge_score(edge_t const *edge)
-{
-    if (palm_tree_frond(edge))
-        return 2 * palm_number(edge->end);
-    else if (palm_tree_arc(edge) &&
-            palm_second_lowest(edge->end) == palm_number(edge->end))
-        return 2 * palm_lowest(edge->end);
-    else if (palm_tree_arc(edge) &&
-            palm_second_lowest(edge->end) < palm_number(edge->end))
-        return 2 * palm_lowest(edge->end) + 1;
-
-
-    fprintf(stderr, "Error should never not hit any of the cases above\n");
-    return -1;
 }
