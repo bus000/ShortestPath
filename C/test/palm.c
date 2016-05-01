b50b02052ace3cbc03e2c338b6120452c3ef144f
Bus000 <magnus@stavngaard.dk>
2016-05-01 10:28:13 +0200

    Testing

--- C/test/palm.c
+++ C/test/palm.c
@@ -4,12 +4,16 @@
 #include <stdlib.h>
 
 void print_palm(digraph_t *graph);
+static linked_list_t spine(vertex_t **w, vertex_t const *u);
 
 int main(int argc, char const *argv[])
 {
     uint32_t i;
     digraph_t graph;
     vertex_id_t vertices[7] = { 0 };
+    linked_list_t spi;
+    actual_list_t *it;
+    vertex_t *v, *w;
 
     vertices_init();
     graph_init(&graph);
@@ -31,6 +35,19 @@ int main(int argc, char const *argv[])
     palm_tree(&graph);
 
     print_palm(&graph);
+
+    /* Print spines. */
+    for (i = 0; i < graph.vertices_len; i++) {
+        v = graph.vertices[i];
+        w = v->outgoing[0].end;
+        spi = spine(&w, v);
+
+        printf("spine: %u -> ", v->unique_id);
+        for (it = spi.start; it != NULL; it = it->next) {
+            printf("%u -> ", ((vertex_t *) it->element)->unique_id);
+        }
+        printf("%u\n", w->unique_id);
+    }
 
     graph_free(&graph);
     vertices_free();
@@ -62,21 +79,17 @@ void print_palm(digraph_t *graph)
                         adjasent->unique_id);
             }
         }
-
-        /*for (j = 0; j < vertex->incoming_len; j++) {*/
-            /*edge = vertex->incoming[j];*/
-            /*adjasent = edge.end;*/
-
-            /*if (palm_tree_arc(&edge)) {*/
-                /*printf("%u -> %u is arc\n", vertex->unique_id,*/
-                        /*adjasent->unique_id);*/
-            /*} else if (palm_tree_frond(&edge)) {*/
-                /*printf("%u -> %u is frond\n", vertex->unique_id,*/
-                        /*adjasent->unique_id);*/
-            /*} else {*/
-                /*printf("%u -> %u is neither\n", vertex->unique_id,*/
-                        /*adjasent->unique_id);*/
-            /*}*/
-        /*}*/
     }
+}
+
+static linked_list_t spine(vertex_t **w, vertex_t const *u)
+{
+    linked_list_t spi = linked_list_init();
+
+    while (palm_number(*w) > palm_number(u)) {
+            linked_list_add_end(&spi, *w);
+            *w = (*w)->outgoing[0].end;
+        }
+
+    return spi;
 }
