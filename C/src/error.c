d6675dbaf742f8057bec05f389a7ca69ff5f1026
Bus000 <magnus@stavngaard.dk>
2016-05-08 18:37:28 +0200

    More flexibility of error codes
    A format string can now be given to the error reporting function and it
    will be printed to stderr before exiting the program.

--- C/src/error.c
+++ C/src/error.c
@@ -1,6 +1,9 @@
+#include "util.h"
 #include "error.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <inttypes.h>
+#include <stdarg.h>
 
 void mem_err(void)
 {
@@ -9,9 +12,16 @@ void mem_err(void)
     exit(EXIT_FAILURE);
 }
 
-void error_code(int error)
+void error_code(error_t error, char const *fmt_msg, ...)
 {
-    fprintf(stderr, "error %d\n", error);
+    va_list ap;
+
+    va_start(ap, fmt_msg);
+
+    fprintf(stderr, "Error %d\n", error);
+    vfprintf(stderr, fmt_msg, ap);
+
+    va_end(ap);
 
     exit(EXIT_FAILURE);
 }
--- C/src/error.h
+++ C/src/error.h
@@ -1,10 +1,14 @@
 #ifndef ERROR_H
 #define ERROR_H
 
+typedef enum {
+    ERR_FILE_OPEN = 1,
+} error_t;
+
 /* Print memory error and exit. */
 void mem_err(void);
 
 /* Print error from error code. */
-void error_code(int error);
+void error_code(error_t error, char const *fmt_msg, ...);
 
 #endif
