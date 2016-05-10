6f2026af29b21148c7437be0e63ce441e654fd6c
Bus000 <magnus@stavngaard.dk>
2016-05-10 09:39:34 +0200

    Error handling

--- C/src/error.c
+++ C/src/error.c
@@ -11,10 +11,38 @@ void error_code(error_t error, char const *fmt_msg, ...)
 
     va_start(ap, fmt_msg);
 
-    fprintf(stderr, "Error %d\n", error);
+    fprintf(stderr, "Error %d - %s\n", error, error_string(error));
     vfprintf(stderr, fmt_msg, ap);
 
     va_end(ap);
 
     exit(EXIT_FAILURE);
+}
+
+void error_report(error_t error, char const *fmt_msg, ...)
+{
+    va_list ap;
+
+    va_start(ap, fmt_msg);
+
+    fprintf(stderr, "Error %d - %s\n", error, error_string(error));
+    vfprintf(stderr, fmt_msg, ap);
+
+    va_end(ap);
+}
+
+char * error_string(error_t error)
+{
+    switch (error) {
+    case ERR_FILE_OPEN:
+        return "ERR_FILE_OPEN";
+    case ERR_NO_MEM:
+        return "ERR_NO_MEM";
+    case ERR_SLEEP:
+        return "ERR_SLEEP";
+    case ERR_FORMAT:
+        return "ERR_FORMAT";
+    default:
+        return "UNKNOWN";
+    }
 }
--- C/src/error.h
+++ C/src/error.h
@@ -3,7 +3,9 @@
 
 typedef enum {
     ERR_FILE_OPEN = 1,
-    ERR_NO_MEM = 2
+    ERR_NO_MEM = 2,
+    ERR_SLEEP = 3,
+    ERR_FORMAT = 4,
 } error_t;
 
 #define MALLOC(var, size) do {                     \
@@ -18,7 +20,13 @@ typedef enum {
         error_code(ERR_NO_MEM, "Out of memory\n"); \
 } while (0);
 
-/* Print error from error code. */
+/* Print error from error code and exit program. */
 void error_code(error_t error, char const *fmt_msg, ...);
+
+/* Print error and return. */
+void error_report(error_t error, char const *fmt_msg, ...);
+
+/* Convert an error code to a string. */
+char * error_string(error_t error);
 
 #endif
