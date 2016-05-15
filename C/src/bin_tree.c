d3608c1fa1e70836a69101715a03073ec5c72321
Bus000 <magnus@stavngaard.dk>
2016-05-16 00:10:12 +0200

    Fixes

--- C/src/bin_tree.c
+++ C/src/bin_tree.c
@@ -1,31 +1,7 @@
 #include "bin_tree.h"
 #include "error.h"
 #include <stdlib.h>
-
-/* Find minimum node in a subtree. */
-/*static tree_container_t * find_min(tree_container_t *current)*/
-/*{*/
-    /*while (current->left != NULL)*/
-        /*current = current->left;*/
-
-    /*return current;*/
-/*}*/
-
-/*static void replace_node_in_parent(tree_container_t *node,*/
-        /*tree_container_t *val)*/
-/*{*/
-    /*if (node->parent != NULL)*/
-        /*if (node->parent->left == node) {*/
-            /*free(node->parent->left);*/
-            /*node->parent->left = val;*/
-        /*} else {*/
-            /*free(node->parent->right);*/
-            /*node->parent->right = val;*/
-        /*}*/
-
-    /*if (val != NULL)*/
-        /*val->parent = node->parent;*/
-/*}*/
+#include <stdio.h>
 
 bin_tree_t bin_tree_init(cmp_t (*cmp_keys)(void const *el1, void const *el2))
 {
@@ -39,11 +15,11 @@ int inline bin_tree_empty(bin_tree_t const *tree)
     return tree->tree == NULL;
 }
 
-void const * bin_tree_find(bin_tree_t const *tree, void const *key)
+int bin_tree_find(int64_t *res, bin_tree_t const *tree, void const *key)
 {
     tree_container_t *next = tree->tree;
 
-    while (tree != NULL) {
+    while (next != NULL) {
         switch (tree->cmp_keys(next->key, key)) {
         case GT:
             next = next->left;
@@ -52,30 +28,31 @@ void const * bin_tree_find(bin_tree_t const *tree, void const *key)
             next = next->right;
             break;
         case EQ:
-            return next->value;
+            *res = next->value;
+            return 0;
         }
     }
 
-    return NULL;
+    return BIN_TREE_NOT_FOUND;
 }
 
-int bin_tree_insert(bin_tree_t *tree, void const *key, void const *value)
+int bin_tree_insert(bin_tree_t *tree, void const *key, int64_t value)
 {
     tree_container_t **next = &tree->tree, *parent = NULL;
 
     while (*next != NULL) {
+        parent = *next;
+
         switch (tree->cmp_keys((*next)->key, key)) {
         case GT:
-            *next = (*next)->left;
+            next = &(*next)->left;
             break;
         case LT:
-            *next = (*next)->right;
+            next = &(*next)->right;
             break;
         case EQ:
             return BIN_TREE_CONFLICT;
         }
-
-        parent = *next;
     }
 
     if ((*next = malloc(sizeof(tree_container_t))) == NULL)
@@ -90,23 +67,30 @@ int bin_tree_insert(bin_tree_t *tree, void const *key, void const *value)
     return 0;
 }
 
-int bin_tree_remove(void const *oldval, bin_tree_t *tree, void const *key)
+int bin_tree_remove(int64_t *oldval, bin_tree_t *tree, void const *key)
 {
     tree_container_t *next = tree->tree, *target = NULL;
+    cmp_t cmp;
 
     if (next == NULL)
         return BIN_TREE_NOT_FOUND;
 
-    while (next != NULL) {
-        switch (tree->cmp_keys(next->key, key)) {
-        case GT:
-            next = next->left;
-            break;
-        case LT:
-            next = next->right;
-            break;
-        case EQ:
+    while (1) {
+        cmp = tree->cmp_keys(next->key, key);
+
+        if (cmp == EQ)
             target = next;
+
+        if (cmp == GT) {
+            if (next->left == NULL)
+                break;
+
+            next = next->left;
+        } else {
+            if (next->right == NULL)
+                break;
+
+            next = next->right;
         }
     }
 
@@ -133,8 +117,62 @@ int bin_tree_remove(void const *oldval, bin_tree_t *tree, void const *key)
     return 0;
 }
 
+int bin_tree_update(int64_t *oldval, bin_tree_t *tree, void const *key,
+        int64_t newval)
+{
+    tree_container_t *next = tree->tree;
+
+    while (next != NULL) {
+        switch (tree->cmp_keys(next->key, key)) {
+        case GT:
+            next = next->left;
+            break;
+        case LT:
+            next = next->right;
+            break;
+        case EQ:
+            *oldval = next->value;
+            next->value = newval;
+
+            return 0;
+        }
+    }
+
+    return BIN_TREE_NOT_FOUND;
+}
+
+static uint32_t bin_tree_size_help(tree_container_t const *cont)
+{
+    if (cont == NULL)
+        return 0;
+
+    return bin_tree_size_help(cont->left) + 1 + bin_tree_size_help(cont->right);
+}
+
+uint32_t bin_tree_size(bin_tree_t const *tree)
+{
+    return bin_tree_size_help(tree->tree);
+}
+
+static void bin_tree_print_help(tree_container_t const *cont)
+{
+    if (cont == NULL)
+        return;
+
+    bin_tree_print_help(cont->left);
+    printf("%p - %" PRIu64 "\n", cont->key, cont->value);
+    bin_tree_print_help(cont->right);
+}
+
+void bin_tree_print(bin_tree_t const *tree)
+{
+    bin_tree_print_help(tree->tree);
+}
+
 void bin_tree_free(bin_tree_t *tree)
 {
+    int64_t value;
+
     while (!bin_tree_empty(tree))
-        bin_tree_remove(tree, tree->tree->key);
+        bin_tree_remove(&value, tree, tree->tree->key);
 }
--- C/src/bin_tree.h
+++ C/src/bin_tree.h
@@ -1,6 +1,8 @@
 #ifndef BIN_TREE_H
 #define BIN_TREE_H
 
+#include <inttypes.h>
+
 #define BIN_TREE_CONFLICT  (1)
 #define BIN_TREE_NOT_FOUND (2)
 
@@ -10,7 +12,7 @@ typedef enum cmp_e {
 
 typedef struct tree_container_s {
     void const *key;
-    void const *value;
+    int64_t value;
 
     /* Everything to the left compares smaller than this element. */
     struct tree_container_s *left;
@@ -36,18 +38,18 @@ typedef struct bin_tree_s {
 bin_tree_t bin_tree_init(cmp_t (*cmp_keys)(void const *el1, void const *el2));
 
 /* Searches the binary tree for the value stored at the node in which the key
- * equals the key given.
+ * equals the key given. The found value is saved to the pointer res.
  *
- * returns NULL    - If the value was not found.
- * returns pointer - Otherwise. */
-void const * bin_tree_find(bin_tree_t const *tree, void const *key);
+ * returns BIN_TREE_NOT_FOUND - If the key was not found.
+ * returns 0                  - Otherwise. */
+int bin_tree_find(int64_t *res, bin_tree_t const *tree, void const *key);
 
 /* Insert a new value in the tree. The value is inserted in the place
  * corresponding to key.
  *
  * returns BIN_TREE_CONFLICT - If the key is already in the map.
  * returns 0                 - Otherwise. */
-int bin_tree_insert(bin_tree_t *tree, void const *key, void const *value);
+int bin_tree_insert(bin_tree_t *tree, void const *key, int64_t value);
 
 /* Find out if a tree is empty.
  *
@@ -60,7 +62,19 @@ int bin_tree_empty(bin_tree_t const *tree);
  *
  * returns BIN_TREE_NOT_FOUND - If the key cannot be found in the tree.
  * returns 0                  - Otherwise. */
-int bin_tree_remove(void const **oldval, bin_tree_t *tree, void const *key);
+int bin_tree_remove(int64_t *oldval, bin_tree_t *tree, void const *key);
+
+/* Replaces the value of a key with the new value given. The old value is saved
+ * to the pointer oldval.
+ *
+ * returns BIN_TREE_NOT_FOUND - If the key could not be found.
+ * returns 0                  - Otherwise. */
+int bin_tree_update(int64_t *oldval, bin_tree_t *tree, void const *key,
+        int64_t newval);
+
+uint32_t bin_tree_size(bin_tree_t const *tree);
+
+void bin_tree_print(bin_tree_t const *tree);
 
 /* Free the resources used by a binary tree, does not free the key and value
  * pointers, that is the users responsibility. */
