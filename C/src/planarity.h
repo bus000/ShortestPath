6dc7b07fad94f7aeab559fb34d5072eb27794dab
Bus000 <magnus@stavngaard.dk>
2016-05-16 00:08:06 +0200

    Add planar representation of graph
    The planar representation works by with each edge saving the clock and
    counter clockwise edges immediately next to the edge in question.

--- C/src/planarity.h
+++ C/src/planarity.h
@@ -4,6 +4,39 @@
 #include "linked_list.h"
 #include "graph.h"
 
+struct planar_vertex_t;
+
+typedef struct planar_edge_s {
+    struct planar_vertex_t *start;
+    struct planar_vertex_t *end;
+
+    /* Stores the edge first encountered if moving clockwise from the edge
+     * (start-end). */
+    struct planar_edge_s *clockwise;
+
+    /* Stores the edge first encountered if moving counter-clockwise from the
+     * edge (start-end). */
+    struct planar_edge_s *counter_clockwise;
+
+    /* Stores the edge first encountered if moving clockwise from the edge
+     * (end-start). */
+    struct planar_edge_s *c_clockwise;
+
+    /* Stores the edge first encountered if moving counter-clockwise form the
+     * edge (end-start). */
+    struct planar_edge_s *c_counter_clockwise;
+} planar_edge_t;
+
+typedef struct planar_vertex_s {
+    vertex_id_t unique_id;
+
+    planar_edge_t *incident_edge;
+} planar_vertex_t;
+
+typedef struct planar_digraph_s {
+
+} planar_digraph_t;
+
 typedef struct block_s {
     linked_list_t a; /* List of vertices. */
     linked_list_t s; /* List of integers. */
