2ca22a807b6035b9f2565a68fc56f1b1246f2831
Bus000 <magnus@stavngaard.dk>
2016-05-23 19:48:32 +0200

    Don't allocate multiple vertices at the same time

--- C/src/vertex.c
+++ C/src/vertex.c
@@ -5,22 +5,19 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-static vertex_t **vertex_pages; /* Array of arrays of vertices. */
-static uint32_t pages_size;
-static long vertices_on_page;
-static long next_vertex;
-
 /* Returns a different unique ID each time the function is called. */
-static vertex_id_t get_unique_id();
+static vertex_id_t get_unique_id()
+{
+    static vertex_id_t unique_id = 0;
+
+    unique_id += 1;
+
+    return unique_id;
+}
 
 void vertices_init(void)
 {
-    vertices_on_page = sysconf(_SC_PAGESIZE) / sizeof(vertex_t);
-    pages_size = 1;
-    next_vertex = 0;
 
-    MALLOC(vertex_pages, sizeof(vertex_t *));
-    MALLOC(vertex_pages[0], sizeof(vertex_t) * vertices_on_page);
 }
 
 vertex_t * new_vertex(void)
@@ -32,16 +29,7 @@ vertex_t * new_vertex_id(vertex_id_t id)
 {
     vertex_t *vertex;
 
-    if (next_vertex >= vertices_on_page) {
-        REALLOC(vertex_pages, sizeof(vertex_t *) * (pages_size + 1));
-        MALLOC(vertex_pages[pages_size], sizeof(vertex_t) * vertices_on_page);
-
-        pages_size += 1;
-        next_vertex = 0;
-    }
-
-    vertex = &(vertex_pages[pages_size-1][next_vertex]);
-    next_vertex += 1;
+    MALLOC(vertex, sizeof(*vertex));
 
     vertex->unique_id = id;
     vertex->outgoing_len = 0;
@@ -57,15 +45,6 @@ vertex_t * new_vertex_id(vertex_id_t id)
     return vertex;
 }
 
-static vertex_id_t get_unique_id()
-{
-    static vertex_id_t unique_id = 0;
-
-    unique_id += 1;
-
-    return unique_id;
-}
-
 void vertex_free(vertex_t *vertex)
 {
     if (vertex->outgoing_size != 0)
@@ -73,24 +52,13 @@ void vertex_free(vertex_t *vertex)
 
     if (vertex->incoming_size != 0)
         FREE(vertex->incoming);
+
+    FREE(vertex);
 }
 
 void vertices_free(void)
 {
-    long i, j, vertices_len;
-    vertex_t *list, *vertex;
 
-    for (i = 0; i < pages_size; i++) {
-        list = vertex_pages[i];
-        vertices_len = i < pages_size -1 ? vertices_on_page : next_vertex;
-        for (j = 0; j < vertices_len; j++) {
-            vertex = &(list[j]);
-            vertex_free(vertex);
-        }
-        FREE(list);
-    }
-
-    FREE(vertex_pages);
 }
 
 void vertex_remove_outgoing(vertex_t *start, vertex_t const *end)
