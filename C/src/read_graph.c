8f7b3e16286da18e7dbd392f2ec7ae767848a6b2
Bus000 <magnus@stavngaard.dk>
2016-05-23 19:47:50 +0200

    Rewrite of reading function

--- C/src/read_graph.c
+++ C/src/read_graph.c
@@ -1,82 +1,76 @@
 #include "graph.h"
 #include "mem_man.h"
+#include "file.h"
+#include "error.h"
+#include "map.h"
 #include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-typedef struct {
-    uint32_t vertex_number;
-} header_t;
+static int cmp_vertex(void const *v1, void const *v2)
+{
+    uint32_t const *vertex1 = v1, *vertex2 = v2;
 
-static header_t read_header(FILE *graph_file);
+    if (*vertex1 < *vertex2)
+        return -1;
+    else if (*vertex1 > *vertex2)
+        return 1;
+    else
+        return 0;
+}
+
+static int hash_vertex(void const *v)
+{
+    uint32_t const *vertex = v;
+
+    return *vertex;
+}
 
 digraph_t read_graph(char const *graph_file)
 {
-    uint32_t i;
     digraph_t graph;
-    FILE *file;
-    char *line = NULL;
-    size_t len = 0, read;
-    /*header_t header;*/
-    vertex_id_t start, end;
-    vertex_t **vertices;
+    char *content, *l;
+    file_t file;
+    uint32_t v0, v1, *newvertex;
+    vertex_t *vertex0, *vertex1;
+    map_t vertices;
 
+    map_init(&vertices, 1024, hash_vertex, cmp_vertex);
+    file_init(&file, graph_file);
     graph_init(&graph);
 
-    file = fopen(graph_file, "r");
-    if (file == NULL) {
-        fprintf(stderr, "File not found %s\n", graph_file);
-        exit(EXIT_FAILURE);
+    content = file_read(&file);
+
+    for (l = strtok(content, "\n"); l != NULL; l = strtok(NULL, "\n")) {
+        if (sscanf(l, "%u\t%u", &v0, &v1) != 2)
+            error_code(ERR_FORMAT, "graph file not correct format '%s' \n", l);
+
+        vertex0 = (vertex_t *) map_get(&vertices, &v0);
+        vertex1 = (vertex_t *) map_get(&vertices, &v1);
+
+        if (vertex0 == NULL) {
+            MALLOC(newvertex, sizeof(*newvertex));
+            *newvertex = v0;
+            vertex0 = new_vertex_id(v0);
+            graph_add_vertex_pointer(&graph, vertex0);
+            map_put(&vertices, newvertex, vertex0);
+        }
+
+        if (vertex1 == NULL) {
+            MALLOC(newvertex, sizeof(*newvertex));
+            *newvertex = v1;
+            vertex1 = new_vertex_id(v1);
+            graph_add_vertex_pointer(&graph, vertex1);
+            map_put(&vertices, newvertex, vertex1);
+        }
+
+        graph_add_edge_pointer(&graph, vertex0, vertex1, 2);
     }
 
-    /*header = read_header(file);*/
-    read_header(file);
-
-    printf("adding vertices\n");
-    /*vertices = malloc(sizeof(vertex_t *) * header.vertex_number);*/
-    vertices = malloc(sizeof(vertex_t *) * 2000000);
-    /*for (i = 0; i < header.vertex_number; i++) {*/
-    for (i = 0; i < 2000000; i++) {
-        vertices[i] = new_vertex();
-        graph_add_vertex_pointer(&graph, vertices[i]);
-    }
-
-    printf("adding edges\n");
-    while ((read = getline(&line, &len, file)) != -1) {
-        sscanf(line, "%u\t%u\n", &start, &end);
-        /*printf("Adding edge %u -> %u\n", start, end);*/
-        graph_add_edge_pointer(&graph, vertices[start], vertices[end], 2);
-        /*graph_add_edge(&graph, start, end, 2);*/
-    }
-
-    fclose(file);
-
-    if (line != NULL)
-        FREE(line);
-
-    FREE(vertices);
+    FREE(content);
+    file_free(&file);
+    map_free(&vertices);
 
     return graph;
-}
-
-static header_t read_header(FILE *graph_file)
-{
-    uint32_t i;
-    char *line = NULL;
-    size_t len;
-    uint32_t nodes, edges;
-    header_t header;
-
-    /* Ignore 2 lines. */
-    for (i = 0; i < 2; i++)
-        getline(&line, &len, graph_file);
-
-    getline(&line, &len, graph_file);
-    sscanf(line, "# Nodes: %u Edges: %u\n", &nodes, &edges);
-    header.vertex_number = nodes;
-
-    /* Ignore 1 line. */
-    getline(&line, &len, graph_file);
-
-    return header;
 }
