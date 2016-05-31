0205952933aed7ea10149220d6ec38a7c70083d8
Bus000 <magnus@stavngaard.dk>
2016-05-31 15:19:31 +0200

    Add line numbers to memory errors

--- src/mem_man.h
+++ src/mem_man.h
@@ -3,27 +3,27 @@
 
 #include <stdlib.h>
 
-#define MALLOC(var, size) do {                     \
-    var = malloc(size);                            \
-    if (var == NULL)                               \
-        error_code(ERR_NO_MEM, "Out of memory\n"); \
+#define MALLOC(var, size) do {                                                 \
+    var = malloc(size);                                                        \
+    if (var == NULL)                                                           \
+        error_code(ERR_NO_MEM, "Out of memory, %s, %d\n", __FILE__, __LINE__); \
 } while (0);
 
-#define REALLOC(var, newsize) do {                 \
-    void *newvar = realloc(var, newsize);          \
-    if (var == NULL)                               \
-        error_code(ERR_NO_MEM, "Out of memory\n"); \
-    var = newvar;                                  \
+#define REALLOC(var, newsize) do {                                             \
+    void *newvar = realloc(var, newsize);                                      \
+    if (var == NULL)                                                           \
+        error_code(ERR_NO_MEM, "Out of memory, %s, %d\n", __FILE__, __LINE__); \
+    var = newvar;                                                              \
 } while (0);
 
-#define CALLOC(var, nmemb, size) do {              \
-    var = calloc(nmemb, size);                     \
-    if (var == NULL)                               \
-        error_code(ERR_NO_MEM, "Out of memory\n"); \
+#define CALLOC(var, nmemb, size) do {                                          \
+    var = calloc(nmemb, size);                                                 \
+    if (var == NULL)                                                           \
+        error_code(ERR_NO_MEM, "Out of memory, %s, %d\n", __FILE__, __LINE__); \
 } while (0);
 
-#define FREE(var) do {                             \
-    free(var);                                     \
+#define FREE(var) do {                                                         \
+    free(var);                                                                 \
 } while (0);
 
 #endif
